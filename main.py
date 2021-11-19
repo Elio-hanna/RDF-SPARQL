60eb7aea65ec0b2d2147611ea09a24fdcb4d7292
elio-hanna <eliohanna2017@outlook.com>
2021-11-19 21:26:44 +0200

    fixed the post from backend

--- main.py
+++ main.py
@@ -1,43 +1,39 @@
+import codecs
 from typing import Optional
 from fastapi import FastAPI, Query
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse, HTMLResponse
-import codecs
-from fastapi.middleware.cors import CORSMiddleware
-import urllib.parse
-import base64
-
 from rdflib import Graph, URIRef
 from rdflib.namespace import RDFS, SKOS
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-# templates = Jinja2Templates(directory='./')
 g = Graph()
 g.parse('leagues.xml', format='xml')
 
-origins = ['*']
+
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
+CORSMiddleware,
+allow_origins=["*"], # Allows all origins
+allow_credentials=True,
+allow_methods=["*"], # Allows all methods
+allow_headers=["*"], # Allows all headers
 )
 
-@app.get('/')
-def index():
-    f=codecs.open('index.html', 'r')
+
+@app.get("/")
+def form_post():
+    f=codecs.open("index.html", 'r')
     html_content = f.read()
     return HTMLResponse(content=html_content, status_code=200)
 
-@app.get('/sparql/{q}')
-async def read_items(q):
+@app.post("/sparql/{q}")
+async def read_items(q = Query(None)):
+    
+    print(q)
     if q:
-        q = urllib.parse.unquote(q)
-        q = q.strip(u'\u200b')
         row = g.query(q)
         json_compatible_item_data = jsonable_encoder(list(row))
-        return JSONResponse(content=json_compatible_item_data)
-    else:
-        return ''
+        print(json_compatible_item_data)
+    return JSONResponse(content=json_compatible_item_data)
