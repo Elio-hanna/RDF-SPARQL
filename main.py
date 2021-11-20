f1da0c8a989db084c59b4b1601f35045fd6f0529
marcalraheb <marcraheb97@gmail.com>
2021-11-20 11:44:34 +0200

    req done

--- main.py
+++ main.py
@@ -1,39 +1,32 @@
 import codecs
-from typing import Optional
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse, HTMLResponse
-from rdflib import Graph, URIRef
-from rdflib.namespace import RDFS, SKOS
 from fastapi.middleware.cors import CORSMiddleware
+from rdflib import Graph
 
 app = FastAPI()
 g = Graph()
-g.parse('leagues.xml', format='xml')
-
-
+g.parse('data/leagues.xml', format='xml')
 
 app.add_middleware(
-CORSMiddleware,
-allow_origins=["*"], # Allows all origins
-allow_credentials=True,
-allow_methods=["*"], # Allows all methods
-allow_headers=["*"], # Allows all headers
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
-
 @app.get("/")
-def form_post():
+def load_page():
     f=codecs.open("index.html", 'r')
     html_content = f.read()
     return HTMLResponse(content=html_content, status_code=200)
 
-@app.post("/sparql/{q}")
-async def read_items(q = Query(None)):
-    
-    print(q)
-    if q:
-        row = g.query(q)
-        json_compatible_item_data = jsonable_encoder(list(row))
-        print(json_compatible_item_data)
+@app.post('/sparql/')
+async def read_items(req: Request):
+    req_json = await req.json()
+    row = g.query(req_json.get('query'))
+    json_compatible_item_data = jsonable_encoder(list(row))
+    print(json_compatible_item_data)
     return JSONResponse(content=json_compatible_item_data)
